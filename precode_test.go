a6bd4972a873e23e70a7a58c0192baf56e7d28e5
Arslan Valiev <arslanfromturkey@gmail.com>
2025-02-09 13:35:54 +0500

    ready2

--- precode_test.go
+++ precode_test.go
@@ -12,7 +12,7 @@ import (
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?city=moscow&count=5", nil) // здесь нужно создать запрос к сервису
+	req := httptest.NewRequest("GET", "/cafe?city=moscow&count=5", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
